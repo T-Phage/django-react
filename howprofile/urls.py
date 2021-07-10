b3a385730636ff4e911a69e9c2a449f9a983932d
T-Phage <samuelgyamfi824@gmail.com>
2021-07-10 12:00:30 +0000

    first commit

--- howprofile/urls.py
+++ howprofile/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^$', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += re_path(r'', TemplateView.as_view(template_name='index.html'))
 
 if settings.DEBUG:
     # urlpatterns += [re_path('', TemplateView.as_view(template_name='index.html'))]
