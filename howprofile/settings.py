5e1dbcf14988bf27df20b3f0e2635a8261225fee
T-Phage <samuelgyamfi824@gmail.com>
2021-07-10 03:40:37 +0000

    first commit

--- howprofile/settings.py
+++ howprofile/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    # 'whitenoise.runserver_nostatic',
     'backend.apps.BackendConfig',
     'django.contrib.sites',
 
